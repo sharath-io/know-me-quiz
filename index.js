27d48111f61f2a6844a69db44923ab312aae3ec0
Sharath <88310969+sharath-io@users.noreply.github.com>
2022-10-22 04:09:32 +0000

    feat: added two levels in the quiz

--- index.js
+++ index.js
@@ -24,6 +24,26 @@ const questions = [
     question: 'Which app i use the most ? ',
     answer: 'whatsapp',
   },
+  {
+    question: 'which college I am from ? ',
+    answer: 'Gitam',
+  },
+  {
+    question: 'what is my branch in my Btech ? ',
+    answer: 'CSE',
+  },
+  {
+    question: 'whats my fav movie of all time ? ',
+    answer: 'RRR',
+  },
+  {
+    question: ' who is my fav film director ? ',
+    answer: 'Sukumar',
+  },
+  {
+    question: 'what is my birthday month ? ',
+    answer: 'December',
+  },
 ];
 
 
@@ -38,22 +58,33 @@ function quiz(question, answer) {
 }
 
 
-function call_quiz(){
+function call_quiz() {
   for (var i = 0; i < questions.length; i++) {
-  console.log('-------------');
-  quiz(questions[i].question, questions[i].answer);
+    console.log('-------------');
+
+    if (!(i === 5 && score < 3)) //checking minimum score at 6th question 
+    {
+      if (i == 5) {
+        console.log(chalk.green('🎉🎉 You have entered level 1'));
+      }
+      quiz(questions[i].question, questions[i].answer);
+    }
+    else {
+      console.log('You didnt score minimum in level 1 so not qualified for level2');
+      break;
+    }
   }
 }
 
 
-function play(){
+function play() {
   const userName = read.question('Whats your name ');
-  console.log(chalk.blue('Welcome to Do-You-Know-Me-Quiz ') + 
-  chalk.green.bold.underline(userName));
-  console.log('lets start the game: ');
+  console.log(chalk.blue('Welcome to Do-You-Know-Me-Quiz ') +
+    chalk.green.bold.underline(userName));
+  console.log('lets start the game: This game has 2 levels \neach having 5 questions, you need to score minimum 3 in level1 to go to level 2 ');
   call_quiz();
   console.log('----------------------');
-  console.log(chalk.blue('Your score is ') + chalk.green.bold(score));
+  console.log(chalk.blue('Your final score is ') + chalk.green.bold(score));
 }
 
 play();
